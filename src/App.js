878742752c265e41a61f68d1a21b6fd8b35eed1b
BilalKeb <90609831+BilalKeb@users.noreply.github.com>
2022-01-28 14:58:10 +0100

    First

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import Home from "./pages/Home";
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import header from "./component/header";
 
 const App = () => {
-  return <div></div>;
+  return <div>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<home/>}/>
+            <Route path="/header" element={<header/>}/>
+        
+          </Routes>
+      </BrowserRouter>
+      </div>;
 };
 
 export default App;
