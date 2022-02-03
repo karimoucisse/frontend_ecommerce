d6f37c80f5afef1be77c34c3bd6de9d18197da4c
BilalKeb <90609831+BilalKeb@users.noreply.github.com>
2022-02-03 09:30:39 +0100

    ModeDeLivraison

--- src/pages/ModeDeLivraison.jsx
+++ src/pages/ModeDeLivraison.jsx
@@ -4,85 +4,126 @@ import Footer from '../components/Footer';
 import Picker from '../components/Picker';
 import styled from 'styled-components';  
 import Card from '../components/Card';
-
- 
+import Button from '../components/Button';
+import Row from '../components/Row'; 
 const Style = styled.div`
     display: flex;
-    /* flex-direction: column; */
+    flex-direction: row;
     justify-content:space-around;
-    margin-top: 10%;
+    margin-top: 10%;   
 `
-
 const Alignement = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
-    margin-top:  10%;
+    margin: 10%;
 `
 const Form = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
-    margin-top:  10%;
+    margin-top:  10%; 
+`
+const Input = styled.div`
+    -ms-transform: scale(1.5); /* IE 9 */
+    -webkit-transform: scale(1.5); /* Chrome, Safari, Opera */
+    transform: scale(1.5);
 `
 const Text = styled.div`
     font-weight: bold;
     font-size: large;
     margin: 50px 15px 0px 25px;
 `
+const Title= styled.div`
+    font-weight: bold;
+    font-size: large;
+    margin: 50px 15px 0px 25px;
+    padding-left: 40%;
+    justify-content: center;
+  `
+  const CalendarStyle= styled.div`
+    display: flex ;
+    justify-content: center;
+    margin-bottom: 5%;
 
+`
+const SelectTime= styled.div`
+    width: 15%;
+    height:15% ;
+    margin: 5%;
+` 
 const ModeDeLivraison = () => {
     return (
         <div>
             <Header/>
-                <h2>Mode de Livraison</h2>
-                <Style>
-                    
+                <Title><h2>Mode de Livraison</h2> </Title>
                     <form>
-                        
-                            <Card  marginTop="10%" width="500px" flex-direction= "column" background="#0a9396"> 
+                        <Style>
+                            <Card  marginTop="10%" width="500px" flex-direction= "column" background="#a3c4f3"> 
                                 <Form>
-                                <input type="radio"  name="subscribe" value="Livraison by Chronofresh "/>
-                                    
+                                    <Input> 
+                                        <input type="radio"  name="rad" value="Livraison by Chronofresh "/> 
+                                    </Input>
                                         <Text>
-                                        <h2><label for="subscribeNews">Livraison by Chronofresh </label> </h2>
-                                        <p>Livraison à domicile en 24/48h 
-                                            pour toute commande 
-                                            passée avant 10h
-                                        </p>    
-                                            <span>15€ TTC</span>
-                                            </Text>
-                                            </Form>   
-                                      <div>
-                                      <img 
-                                      src = "https://tinyurl.com/yckw5h4x" 
-                                      alt="popo "
-                                      style={{ height:"250px" ,marginTop:"80px"}}/>
-                                      </div>
+                                            <h2><label for="subscribeNews">Livraison by Chronofresh </label> </h2>
+                                                <br/>
+                                                    <p>Livraison à domicile en 24/48h pour toute commande  passée avant 10h</p>    
+                                                        <span>15€ TTC</span>
+                                        </Text>
+                                </Form>   
+                                    <img 
+                                        src = "https://tinyurl.com/yckw5h4x" 
+                                        alt="popo "
+                                        style={{ height:"250px" ,marginTop:"80px"}}/>
                             </Card>
+                            <Card marginTop="10%" width="500px" flex-direction= "column" background="#a3c4f3">
+                                <Form> 
+                                    <Input>
+                                        <input type="radio" name="rad" value="So Fresh"/> 
+                                    </Input>
+                                        <Text> 
+                                            <h2><label for="subscribeNews">Livraison by Sofresh </label></h2>
+                                                <br/>
+                                                    <p>Points relais frais à Paris
+                                                    domicile en 24/48h 
+                                                    pour toute commande 
+                                                    passée avant 10h
+                                                    </p>
+                                                        <span> 18.60€ TTC</span>
+                                        </Text>
+                                </Form>         
+                                                <img 
+                                                src = "https://tinyurl.com/2p8r9frh" 
+                                                alt="popo "
+                                                style={{ height:"200px" ,marginTop:"100px"}}/>
+                            </Card> 
+                        </Style> 
                     </form>
-                    <form>
-                        
-                        <Card marginTop="10%" width="500px" flex-direction= "column" background="#0a9396">
-                            <Form>  
-                                <input type="radio" />
-                                    <label for="subscribeNews">Livraison by Sofresh </label>
-                                        <p>Points relais frais à Paris</p>
-                                            <span> 18.60€ TTC</span>
-                            </Form>                
-                        </Card> 
-                     </form>
-                     </Style>  
                         <Alignement> 
-                            <label for="name">Si vous voulez nous laisser un message à propos de votre commande, merci de
+                                    <h2><label for="name">Si vous voulez nous laisser un message à propos de votre commande, merci de
                                     bien vouloir le renseigner dans le champ ci-contre.
-                            </label>
-                                <textarea id="story" name="story"
+                                    </label> </h2> 
+                                    <textarea id="story" name="story"
                                     rows="5" cols="33">
-                                        Message à propos de votre commande...
-                                </textarea>
-                        </Alignement>    
-                        <Picker/>
+                                    Message à propos de votre commande...
+                                    </textarea>
+                        </Alignement>  
+                            <CalendarStyle>
+                                <Picker/>
+                                    <SelectTime>      
+                                        <h3><label for="appt"> Choisisez votre heure de Livraison </label>
+                                            <input type="time" id="appt" name="appt"  
+                                                min="09:00" max="13:00" required/> 
+                                                    <small>9h00-13h00</small>     </h3>  
+                                    </SelectTime> 
+                            </CalendarStyle>
+                                <Row alignItems="center" >
+                                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                                    Omnis veniam culpa porro pariatur debitis perferendis impedit quis tenetur?
+                                    Maiores commodi repellat a incidunt molestias quasi ratione,
+                                    esse modi nobis at.</p>
+                                    <Button background= "endregion"  height="100px" width="200px" margin="150px 40px 30px 80px" >Payer</Button> 
+                            </Row>
                         <Footer/>
         </div>
     );
--- src/pages/Panier.jsx
+++ src/pages/Panier.jsx
@@ -1,14 +1,14 @@
 import Container from "../components/Container"
 import Header from "../components/Header"
 import Footer from "../components/Footer"
-import styled from "styled-components";
-import Button from "../components/Button";
-import Section from "../components/Section";
+// import styled from "styled-components";
+// import Button from "../components/Button";
+// import Section from "../components/Section";
 import EmptyBasket from "../components/EmptyBasket";
 
-const Title = styled.h1`
-    text-align: center;
-`
+// const Title = styled.h1`
+//     text-align: center;
+// `
 const Panier = () => {
     return (
         <Container>
