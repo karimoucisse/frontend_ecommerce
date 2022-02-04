c967bd97143e661655227f7f9addb6ce5688d329
EloiGry <eloi.grychta@gmail.com>
2022-02-04 17:27:03 +0100

    modif

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,6 +11,8 @@ import Container from "../components/Container";
 import { useNavigate } from "react-router-dom";
 import QuantityButton from "../components/QuantityButton";
 import CommentCaMarche from "../components/CommentCaMarche";
+import {useEffect, useState} from 'react';
+import getCategories from "../api/getCategories";
 
 const ImageContainer = styled.div`
     height: 600px;
@@ -29,67 +31,19 @@ const ProductTitle = styled.h2`
 `
 
 const Home = () => {
-    const navigate = useNavigate();
-    const ProductListImages = [
-        [
-            {
-                source : "https://www.lecomptoirdanais.com/89-home_default/bar-entier-elevage-2-pieces.jpg",
-                borderRadius: "20px 0 0 0",
-                width: "400px",
-                content: "Nos  poissons entiers"
-            },
-            {
-                source:"https://res.cloudinary.com/hv9ssmzrz/image/fetch/c_fill,f_auto,h_600,q_auto,w_800/https://s3-eu-west-1.amazonaws.com/images-ca-1-0-1-eu/tag_photos/original/2692/coquillages_flickr_11444005806_b82506677e_b.jpg",
-                borderRadius: "0 0 0 0",
-                width: "400px",
-                content: "Nos coquillages"
-            },
-            {
-                source: "https://www.boutique-paon.fr/4479-large_default/crevette-elevage-cuit-80-100-equateur-ou-autre-pays.jpg",
-                borderRadius: "0 20px 0 0",
-                width: "400px",
-                content: "Nos crustacés"
-            }
-        ],
-        [
-            {
-                source: "https://res.cloudinary.com/hv9ssmzrz/image/fetch/c_fill,f_auto,h_488,q_auto,w_650/https://images-ca-1-0-1-eu.s3-eu-west-1.amazonaws.com/photos/original/1196/homard-pixabay-lobster-4952603_1280.jpg", 
-                borderRadius: "0 0 0 20px",
-                width: "400px",
-                content: "Nos produit festif"
+const [categories, setCategories] = useState([])
 
-            },
-            {
-                source: "https://larecette.net/wp-content/uploads/2019/11/iStock-469781786-1152x768.jpg",
-                borderRadius: "0 0 20px 0",
-                width: "800px",
-                content: "Nos fillet de poissons"
-            }
-        ]
-    ]
-    const onclickNavigate = (content) => {
-        switch (content) {
-            case 'Nos  poissons entiers':
-                navigate("/poisson_entiers")
-                break;
-            case 'Nos coquillages':
-                navigate("/coquillages")
-                break;
-            case 'Nos crustacés':
-                navigate("/crustaces")
-                break;
-            case 'Nos produit festif':
-                navigate("/produit_festif")
-                break;
-            case 'Nos fillet de poissons':
-                navigate("/fillet_de_poissons")
-                break;
-            
-            default:
-            navigate("/")
-        }
-        console.log("hello");
-    }
+    useEffect(() => {
+        fetchCategories()
+      }, [])
+    
+      const fetchCategories = async () => {
+          const categories = await getCategories()
+          setCategories(categories)
+          console.log("My categories", categories)
+      }
+    
+   
     return ( 
         <Container>
             <Header/>
@@ -102,29 +56,27 @@ const Home = () => {
                 <h1>Que cherchez vous ?</h1>
                 <ImageContainer>
                     
-                        {ProductListImages.map((listImage, rowIndex) => {
-                            return <Row position = "relative" key={rowIndex}> 
-                                {listImage.map((element, index) => {
-                                return (
+                        {categories.map(category => {
+                            return <Row position = "relative" key={category.name}> 
                                         <Card
-                                            key={index}
-                                            width= {element.width} 
+                                            key={category.name}
+                                            width= {category.width} 
                                             height= "300px" 
                                             position= "relative"
-                                            onClickAction = {() => onclickNavigate(element.content)}
+                                            // onClickAction = 
                                         >
                                             <Image 
-                                                source= {element.source}
-                                                borderRadius={element.borderRadius}
-                                                width = {element.width ? element.width : "400px"}
+                                                source= {category.image}
+                                                borderRadius="20px"
+                                                width = "400px"
                                                 isShadow
                                             />
-                                            <ProductTitle>{element.content}</ProductTitle>
+                                            <ProductTitle>{category.name}</ProductTitle>
                                         </Card>
-                                    )
-                            })}
                             </Row>
                         })}
+                            
+                        
                      
                 </ImageContainer>
             </Section>
