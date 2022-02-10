55e8d3b9e6fef1018a5d06e0e240f62d4c86f0b0
Nascimento95 <marco-pereira@hotmail.fr>
2022-02-10 17:44:52 +0100

    les zero mis sur le kilo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -174,7 +174,7 @@ const Home = () => {
                                     <Image source={product.image} alt={product.name} height='180px' width='250px' borderRadius='20px 20px 0px 0px'/>
                                     <H> {product.name} </H>
                                     <Flex> 
-                                        <p> Prix au kilo : {product.kiloPrice}€ </p>
+                                        <p> Prix au kilo : {product.kiloPrice}0€ </p>
                                         
                                     </Flex> 
                                 </Card>
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -93,7 +93,7 @@ const Product = () => {
                         <Content>
                             <div>
                                 <h3> {product.name} </h3>
-                                <Paragraph> {product.kiloPrice}€ kilo</Paragraph>
+                                <Paragraph> {product.kiloPrice}0€ kilo</Paragraph>
                                 <Paragraph> Prix par pièce : {product.pricePerPiece}€ </Paragraph>
                                 <QuantityButton 
                                     margin = "0" 
