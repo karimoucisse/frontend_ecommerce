16433ba51539334b44886813edcb00fcffbdc5db
BilalKeb <90609831+BilalKeb@users.noreply.github.com>
2022-02-10 11:14:42 +0100

    Recup

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -10,7 +10,7 @@ const Container = styled.main`
     height: 100vh;
     width: 100vw;
     position: relative;
-    margin-bottom: -3% ;
+    margin-bottom: -2.8% ;
 `
 
 const MainTitle = styled.h1`
@@ -19,7 +19,7 @@ const MainTitle = styled.h1`
     align-items: center;
     align-self: center;
     font-size: 80px;
-    top: 70%;
+    top: 80%;
     left: 50%;
 `
 const Title = styled.h1`
@@ -30,7 +30,6 @@ const Title = styled.h1`
     font-size: 80px;
     top: 10%;
     left: 50%;
-
 `
 const NotFound = () => {
     return (
@@ -38,7 +37,7 @@ const NotFound = () => {
             <Header />
             <Container>
                 <Title> 404 Not Found</Title>
-                <img src ="https://tinyurl.com/y2fshhtu" alt="Pecheur" style={{height:"100"}}></img>
+                <img src ="https://tinyurl.com/y2fshhtu" alt="Pecheur" style={{width:"100%"}}></img>
                 <MainTitle>No fish in this fishing area .</MainTitle>
             </Container>
             <Footer />
